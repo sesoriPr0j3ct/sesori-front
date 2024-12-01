3cac2aff8afd9b0dbd1e1e5659c7195aad975921
minhee231 <minhee231>
2024-12-01 23:14:53 +0900

    :gem:style:dark theme 추가 및 적용

--- src/components/MainPost.vue
+++ src/components/MainPost.vue
@@ -1,30 +1,26 @@
 <template>
-    <v-container color="" class="custom-container">
-    <!-- <v-container id="container"> -->
-      {{ test }}
-      <v-table>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>작성일</th>
-            <th>추천수</th>
-            <th>조회수</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in test" :key="item.postIdx">
-            <td>{{ item.postIdx }}</td>
-            <td>{{ item.contentTitle }}</td>
-            <td>{{ item.userName }}</td>
-            <td>{{ item.createdAt }}</td>
-            <td>{{ item.likeCount }}</td>
-            <td>{{ item.viewCount }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-container>
+    <v-table class="theme-surface">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>제목</th>
+          <th>작성자</th>
+          <th>작성일</th>
+          <th>추천수</th>
+          <th>조회수</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in test" :key="item.postIdx">
+          <td>{{ item.postIdx }}</td>
+          <td>{{ item.contentTitle }}</td>
+          <td>{{ item.userName }}</td>
+          <td>{{ item.createdAt }}</td>
+          <td>{{ item.likeCount }}</td>
+          <td>{{ item.viewCount }}</td>
+        </tr>
+      </tbody>
+    </v-table>
   </template>
   
   
@@ -49,17 +45,5 @@
     },
   };
   </script>
-  
-  <style scoped>
-  .custom-container {
-    background-color: rgb(var(--v-theme-primary));
-    color: rgba(var(--v-theme-on-primary));
-    padding: 16px;
-  }
 
-  #container {
-  background-color: rgb(var(--v-theme-primary));
-  color: rgba(var(--v-theme-on-primary), 0.8);
-}
-  </style>
   
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,5 @@
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
-// Vuetify
-// Vuetify 설정 파일
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 
@@ -18,6 +14,7 @@ export default createVuetify({
           surface: '#f5f5f5',
           primary: '#1976D2',
           secondary: '#FF5722',
+          tertiary: '#FF5722',
         },
       },
       dark: {
@@ -25,8 +22,9 @@ export default createVuetify({
         colors: {
           background: '#121212',
           surface: '#212121',
-          primary: '#BB86FC',
-          secondary: '#03DAC6',
+          primary: '#424242',
+          secondary: '#616161',
+          tertiary: '#757575',
         },
       },
     },
