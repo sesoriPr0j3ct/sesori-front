fa500ffea5b609f2d2dece494f879946794d1b04
minhee231 <minhee231>
2024-12-01 23:17:12 +0900

    :hammer:refactor: Hello World 컴포넌트 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <v-app color="background">
     <v-main>
-      <!-- <HelloWorld/> -->
        <MainPost/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import MainPost from './components/MainPost.vue';
 
 export default {
